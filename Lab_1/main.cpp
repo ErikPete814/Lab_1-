c2f8b3244117e78468e4d9789cd4ed9fb7f8f53c
Erik Petersen <Erikpete7@gmail.com>
2026-06-29 18:07:27 -0700

    Initial commit

--- Lab_1/main.cpp
+++ Lab_1/main.cpp
@@ -1,14 +1,8 @@
-//
-//  main.cpp
-//  Lab_1
-//
-//  Created by Erik Petersen on 6/29/26.
-//
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return EXIT_SUCCESS;
+int main() {
+    cout <<"Hello World!"<< endl;
+    return 0;
+    
 }
